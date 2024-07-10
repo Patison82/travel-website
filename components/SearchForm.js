3ceb65ed1e2f3abe5e88efb5500020c8cf54a666
Patrycja Wozniewicz <patrycja.wozniewicz@gmail.com>
2024-07-10 13:27:01 +0200

    clors  added + responsivitate from search box

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -2,25 +2,21 @@
 
 import { useState } from "react";
 
-
 const SearchForm = () => {
   const [tripType, setTripType] = useState("return"); // State for trip type (return or one-way)
   const [addNearbyFrom, setAddNearbyFrom] = useState(false); // State for "Add nearby airports" checkbox for departure city
   const [addNearbyTo, setAddNearbyTo] = useState(false); // State for "Add nearby airports" checkbox for destination city
-
+  const [directFlightsOnly, setDirectFlightsOnly] = useState(false);
 
   return (
-   
-    <div className="bg-amber-200 shadow-2xl p-10 rounded-lg max-w-6xl mx-auto mt-4 mb-10 absolute bottom-0 left-0 right-0">
-      
+    <div className="bg-amber-200 shadow-2xl p-6 rounded-lg max-w-6xl mx-auto mt-8 sm:mb-10 sm:fixed sm:bottom-0 sm:left-0 sm:right-0">
       {/* Title Section */}
-      <h2 className=" text-4xl font-semibold mb-6 text-teal-800 text-center  ">
-        Where to  next  ?
+      <h2 className=" text-4xl font-semibold mb-6 text-teal-800 text-center sm:text-4xl ">
+        Where to next ?
       </h2>
 
-
       {/* Trip Type Section */}
-      <div className="flex mb-8 space-x-10">
+      <div className="flex flex-col mb-8 sm:space-x-10 sm:flex-row sm:space-y-0 ">
         <label className="inline-flex  items-center text-gray-800">
           <input
             type="radio"
@@ -30,8 +26,7 @@ const SearchForm = () => {
             className="form-radio text-blue-600"
           />
 
-           
-          <span className="ml-2">RETURN</span>
+          <span className="ml-2 text-">RETURN</span>
         </label>
         <label className="inline-flex items-center text-gray-800">
           <input
@@ -42,17 +37,13 @@ const SearchForm = () => {
             className="form-radio text-blue-600"
           />
 
-
-          <span className="ml-2">ONE-WAY</span>
+          <span className="ml-2 ">ONE-WAY</span>
         </label>
       </div>
 
-
-
-        {/* Cities and Dates Section */}
-      <div className="flex mb-6 space-x-4">
-        <div className="flex flex-col w-1/4">
-
+      {/* Cities and Dates Section */}
+      <div className="flex flex-col sm:flex-row mb-6 space-y-4 sm:space-y-0 sm:space-x-4">
+        <div className="flex flex-col sm:w-1/4 w-full ">
           {/* Departure city */}
           <label className="font-semibold mb-2 text-gray-800">From</label>
           <div className="relative">
@@ -61,7 +52,6 @@ const SearchForm = () => {
               placeholder="Departure city"
               className="p-2 border  border-green-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 w-full"
             />
-
 
             {/* "Add nearby airports" Section */}
             <label className="inline-flex items-center mt-2 text-sm text-gray-600">
@@ -76,9 +66,8 @@ const SearchForm = () => {
           </div>
         </div>
 
-
-           {/* Destination City */}
-        <div className="flex flex-col w-1/4">
+        {/* Destination City */}
+        <div className="flex flex-col sm:w-1/4 w-full">
           <label className="font-semibold mb-2 text-gray-800">To</label>
           <div className="relative">
             <input
@@ -100,19 +89,33 @@ const SearchForm = () => {
           </div>
         </div>
 
-        <div className="flex flex-col w-1/4">
+        {/* Departure Date */}
+        <div className="flex flex-col sm:w-1/4 w-full">
           <label className="font-semibold mb-2 text-gray-800">Depart</label>
           <input
             type="date"
-    
             className="p-2 border  border-green-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300 w-full"
           />
-        </div>
 
+          {/* Direct Flights Only Section */}
+          <div className="inline-flex items-center mb-6">
+            <label className="inline-flex items-center mt-2 text-sm text-gray-600 ">
+              <input
+                type="checkbox"
+                className="form-checkbox text-blue-600"
+                checked={directFlightsOnly}
+                onChange={() => setDirectFlightsOnly(!directFlightsOnly)}
+              />
+              <span className="ml-2 mr-[8rem] text-xs ">
+                Direct flights only
+              </span>
+            </label>
+          </div>
+        </div>
 
         {/* Return Date (conditionally rendered based on trip type) */}
         {tripType === "return" && (
-          <div className="flex flex-col w-full md:w-1/4">
+          <div className="flex flex-col w-full sm:w-1/4">
             <label className="font-semibold mb-2 text-gray-800">Return</label>
             <input
               type="date"
@@ -122,24 +125,21 @@ const SearchForm = () => {
         )}
       </div>
 
-
       {/* Passengers and Class Section */}
-      <div className="flex  space-x-3">
-        <div className="flex flex-col w-1/6">
-
-          {/* Number of Passengers */}
+      <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-3">
+        {/* Number of Passengers */}
+        <div className="flex flex-col sm:w-1/6 w-full">
           <label className="font-semibold mb-2 text-gray-800">Passengers</label>
           <input
             type="number"
             min="0"
             defaultValue="1"
-            className="p-2  border border-green-500 rounded-lg focus:outline-none  focus:ring-3 focus:ring-blue-400 transition duration-300 w-full"
+            className="p-1.5  border border-green-500 rounded-lg focus:outline-none  focus:ring-3 focus:ring-blue-400 transition duration-300 w-full"
           />
         </div>
 
-
-         {/* Class Selection */}
-        <div className="flex flex-col w-1/6">
+        {/* Class Selection */}
+        <div className="flex flex-col sm:w-1/6 w-full">
           <label className="font-semibold mb-2 text-gray-800">Class</label>
           <select className="p-2  border border-green-500 rounded-lg focus:outline-none focus:ring-3 focus:ring-blue-500 transition duration-300 w-full">
             <option value="economy">Economy</option>
@@ -147,11 +147,10 @@ const SearchForm = () => {
             <option value="first">First</option>
           </select>
         </div>
-    </div>
-
+      </div>
 
       {/*  Search Button Section */}
-      <div className="flex justify-end ">
+      <div className="flex justify-end mt-6">
         <button className="px-6 py-3 bg-teal-500 text-amber-50 font-semibold rounded-lg  hover:bg-emerald-400 transition duration-300 shadow-lg ">
           Search Flights
         </button>
