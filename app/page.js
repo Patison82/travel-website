9f84cdd932768a3292cfef5b68d1acf2b4a137e9
Hossaini <1989hossaini1@gmail.com>
2024-07-10 15:38:24 +0200

    added new component

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import SearchForm from "@/components/SearchForm";
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center min-h-screen p-4">
+      <main>
         
         <SearchForm />
       </main>
