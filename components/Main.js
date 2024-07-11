d6885c6c6a17d9608f3fb65a7107b14c1ab3caf5
Anastasios Aintinidis <taseos123@hotmail.com>
2024-07-11 15:44:22 +0200

    add main h1 button not completed

--- components/Main.js
+++ components/Main.js
@@ -1,6 +1,9 @@
 "use client";
 import { motion, useScroll, useTransform } from "framer-motion";
+// import Link from "next/link";
 import React, { useRef } from "react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Parallax() {
   const ref = useRef(null);
@@ -10,7 +13,6 @@ export default function Parallax() {
   });
 
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "40%"]);
-  const backgroundY2 = useTransform(scrollYProgress, [0, 1], ["0%", "50%"]);
   const textY = useTransform(scrollYProgress, [0, 1], ["0", "200%"]);
 
   return (
@@ -18,12 +20,16 @@ export default function Parallax() {
       ref={ref}
       className="w-full h-screen overflow-hidden relative grid place-items-center"
     >
-      <motion.h1
+      <motion.div
         style={{ y: textY }}
-        className="font-bold text-white text-7xl md:text-9xl relative z-30 drop-shadow-2xl"
+        className="font-bold text-white text-5xl md:text-7xl relative z-30 drop-shadow-2xl w-full lg:w-1/2 text-center opacity-85"
       >
-        PASAVA
-      </motion.h1>
+        <h1>Pavasa Travel Service</h1>
+        <p></p>
+        {/* <Link href={"about"} className="z-30 relative">
+          <FontAwesomeIcon icon={faArrowDown} />
+        </Link> */}
+      </motion.div>
 
       <motion.div
         className="absolute inset-0 z-10"
