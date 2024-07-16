185560bc4dd76f03c83ba9164ac4706016aa94c5
Samira <samira.hni1989@gmail.com>
2024-07-16 13:22:49 +0200

    signin&sign-up mit clerk wurde erstellt

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,13 @@
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import "./globals.css";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,11 +17,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
         <Header />
         {children}
       </body>
     </html>
+
+  </ClerkProvider>
   );
 }
---
+++ app/welcome/page.js
@@ -0,0 +1,13 @@
+
+
+
+const WelcomePage= () => {
+  return (
+    <div>
+      <p className="text-center pt-32 text-2xl font-bold">One Key</p>
+
+
+    </div>
+  )
+}
+export default WelcomePage
