9e401fdf5391dc031b1c0b83593b35cde5f6cdcb
Anastasios Aintinidis <taseos123@hotmail.com>
2024-07-16 16:00:33 +0200

    add gif form background

--- components/GifBackground.js
+++ components/GifBackground.js
@@ -5,7 +5,7 @@ const GifBackground = ({ children }) => {
     <div className="relative w-full hide  overflow-hidden">
       <div
         className="absolute inset-0 z-0 bg-cover bg-center"
-        style={{ backgroundImage: "url('/200.gif')" }}
+        style={{ backgroundImage: "url('/background.gif')" }}
       ></div>
       <div className="relative z-10">{children}</div>
     </div>
