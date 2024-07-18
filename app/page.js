5830b8637682cf8bccc42879499c0603f7d53f61
Anastasios Aintinidis <taseos123@hotmail.com>
2024-07-18 16:15:15 +0200

    add hotel section

--- app/page.js
+++ app/page.js
@@ -3,19 +3,17 @@ import Parallax from "@/components/Main";
 import FlightSearchForm from "@/components/FlightSearchForm";
 import HotelSearchForm from "@/components/HotelSearchForm";
 import Icons from "@/components/Icons";
- 
-
-
-
+import HotelSection from "@/components/HotelSection";
 
 export default function Home() {
   return (
     <>
-      <main >
+      <main>
         <Parallax />
         <FlightSearchForm />
         <Carousel />
         <HotelSearchForm />
+        <HotelSection />
         <Icons />
       </main>
     </>
