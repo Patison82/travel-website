eb9274bb3680e053ec7ee5bae387a6b5950cf4ad
Hossaini <1989hossaini1@gmail.com>
2024-07-23 16:30:27 +0200

    search form frontent erledigt

--- app/api/flights/route.js
+++ app/api/flights/route.js
@@ -1,35 +1,44 @@
-
 import flightModel from "@/models/flightModel";
 import connectDB from "@/utils/database/connectDB";
 import { NextResponse } from "next/server";
 
+export async function GET(req) {
+  // const { from, to } = req.query;
+  // console.log(req.query);
+
+ 
+
+  const {searchParams}=new URL(req.url);
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+  const depart = searchParams.get('depart')
+  console.log(depart);
 
 
-export async function GET() {
+  // const params = new URLSearchParams()
+  // params.set('from',from)
+  // params.set('to',to)
+
+  const query = {};
+
+  if (from) {
+    query.from = { $regex: new RegExp(from, "i") };
+  }
+
+  if (to) {
+    query.to = { $regex: new RegExp(to, "i") };
+  }
+  if (depart) {
+    query.depart = { $regex: new RegExp(depart, "i") };
+  }
+  
   await connectDB();
-
   try {
-    const flights = await flightModel.find({});
-    return NextResponse.json({ flights }, { status: 200 });
+    // console.log(query);
+    const flightsData = await flightModel.find(query);
+    return NextResponse.json({ flightsData }, { status: 200 });
   } catch (error) {
     console.error("Server Error", error);
     return NextResponse.json({ message: "GET Error!" }, { status: 500 });
   }
 }
-
-
-
-
-// export async function POST(req){
-
-//   await connectDB()
-//   try {
-//     const body = await req.json();
-
-//     await flightModel.create(body)
-
-//   } catch (error) {
-//     console.error("Server Error", error);
-//     return NextResponse.json({ message: "GET Error!" }, { status: 500 });
-//   }
-// }
